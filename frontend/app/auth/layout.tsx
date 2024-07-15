174569968de1673564a9004b484c87c5f42c1ac4
Manish Devrani <manishdevrani777@gmail.com>
2024-07-16 00:46:28 +0530

    delete frontend

--- frontend/app/auth/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function AuthLayout({ 
-  children 
-}: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <div className="h-full flex items-center justify-center">
-      {children}
-    </div>
-  )
-}
